10165acc24d13535dbfa4b7c1be59a515d28e2a2
noella <noelladsa@gmail.com>
2015-06-26 22:45:24 -0400

    Added indices to checklist table

--- db_ops/deploy_sql/checklists.sql
+++ db_ops/deploy_sql/checklists.sql
@@ -17,6 +17,8 @@ CREATE TABLE checklists(
 "group_id" text,
 "primary_checklist_flag" text,
 "sightings" jsonb);
-
+create index checklists_gin on checklists using gin(sightings);
+create index long_in on checklists(longitude); 
+create index lat_in on checklists(latitude); 
 
 
